c1eb733c38b4bcb4aba9f208cc9392285ce52056
mvaldez <markvaldez@gmail.com>
2018-02-12 19:54:01 -0600

    switched to pytest

--- test_solutions.py
+++ test_solutions.py
@@ -1,12 +1,13 @@
-import unittest
+# import unittest
 import solutions
+import pytest
 
 
-class SolutionTest(unittest.TestCase):
+class TestSolutions(object):
 
     def test_find_words(self):
         result = solutions.find_words(["Hello", "Alaska", "Dad", "Peace"])
-        self.assertEqual(["Alaska", "Dad"], result)
+        assert ["Alaska", "Dad"] == result
 
     def test_trimBST(self):
         root = solutions.TreeNode(3)
@@ -15,7 +16,7 @@ class SolutionTest(unittest.TestCase):
         solutions.insert_val(root, 2)
         solutions.insert_val(root, 1)
 
-        self.assertEqual(solutions.breadth_first(solutions.trimBST(self, root, 1, 3)), [3, 2, 1])
+        assert solutions.breadth_first(solutions.trimBST(self, root, 1, 3)) == [3, 2, 1]
 
     def test_jewels(self):
         J1 = "aA"
@@ -24,8 +25,8 @@ class SolutionTest(unittest.TestCase):
         J2 = "z"
         S2 = "ZZ"
 
-        self.assertEqual(solutions.numJewelsInStones(self, J1, S1), 3)
-        self.assertEqual(solutions.numJewelsInStones(self, J2, S2), 0)
+        assert solutions.numJewelsInStones(self, J1, S1) == 3
+        assert solutions.numJewelsInStones(self, J2, S2) == 0
 
     def test_callPoints(self):
         input_1 = ["5","2","C","D","+"]
@@ -33,9 +34,9 @@ class SolutionTest(unittest.TestCase):
         expected_1 = 30
         expected_2 = 27
 
-        self.assertEqual(solutions.callPoints(self, input_1), expected_1)
-        self.assertEqual(solutions.callPoints(self, input_2), expected_2)
+        assert solutions.callPoints(self, input_1) == expected_1
+        assert solutions.callPoints(self, input_2) == expected_2
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
